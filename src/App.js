399838faac7ec03ed2d091af4d489d2b91e40660
ShezanInstaWebWorks <135579330+ShezanInstaWebWorks@users.noreply.github.com>
2024-01-15 11:40:30 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TableWidget from './components/TableWidget';
+import EnhancedTable from './components/NewTable';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TableWidget/> */}
+      <EnhancedTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewTable.jsx
@@ -0,0 +1,301 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import axios from 'axios';
+
+
+
+
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'id',
+    numeric: false,
+    disablePadding: true,
+    label: 'ID',
+  },
+  {
+    id: 'date',
+    numeric: true,
+    disablePadding: false,
+    label: 'DATE/TIME',
+  },
+  {
+    id: 'info',
+    numeric: true,
+    disablePadding: false,
+    label: 'INFO',
+  },
+  {
+    id: 'response',
+    numeric: true,
+    disablePadding: false,
+    label: 'RESPONSE',
+  },
+ 
+];
+
+function EnhancedTableHead(props) {
+  const {  order, orderBy, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    
+    <TableHead >
+      <TableRow >
+        
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              <h4>{headCell.label}</h4>
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  // onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+
+
+
+
+export default function EnhancedTable() {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('date');
+ 
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const fetchData = async () => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://api.easy-pluginz.com/admin/v2/utils/responselogs",
+      headers: {
+        orgid: "10698000000106085",
+      },
+    };
+
+    const res = await axios.request(config);
+
+    console.log("obj", res?.data.data);
+    const formattedData = res?.data?.data.map((row) => ({
+      ...row,
+      created_at: new Date(row.created_at).toLocaleDateString(undefined, {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+    // second: "2-digit",
+      }),
+    }));
+    // formattedData.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+// console.log("shezan",formattedData);
+    setData(formattedData);
+    setLoading(false);
+  };
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+  React.useEffect(() => {
+  
+    handleRequestSort(null, 'date', 'asc');
+  }, []); 
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+  const compareDates = (a, b, order) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+  
+    if (order === 'asc') {
+      return dateA - dateB;
+    } else {
+      return dateB - dateA;
+    }
+  };
+  
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+
+    const visibleRows = React.useMemo(
+      () =>
+        stableSort(data, (a, b) =>
+          orderBy === 'date'
+            ? compareDates(a, b, order)
+            : getComparator(order, orderBy)
+        ).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+      [data,order, orderBy, page, rowsPerPage]
+    );
+  if (loading) {
+    
+    return <div>Loading...</div>;
+  }
+  
+  
+
+  return (
+    <Box sx={{ width: '80%',padding:"50px 50px 50px 150px" }}>
+      <Paper sx={{ width: '100%', mb: 2,pl:2 }}>       
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+
+                return (
+                  <TableRow
+                    hover
+                    
+                    key={row.id}
+                    // sx={{ cursor: 'pointer' }}
+                  >
+                    
+                    
+                    <TableCell
+                      component="th"
+                      // id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.id}
+                    </TableCell>
+                    <TableCell align="right">{row.created_at}</TableCell>
+                    <TableCell align="right">{row.req}</TableCell>
+                    <TableCell align="right">{row.res}</TableCell>
+                    
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10,15,25]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
+}
